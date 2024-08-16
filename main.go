6021bc2514620b88be55c84a054455da975da65b
Rui Lopes <rgl@ruilopes.com>
2024-08-16 19:40:58 +0100

    style the table to use all the available width and word wrap the data

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ table {
 	margin-bottom: 1.25rem;
 	padding: .75rem 1.25rem;
 	text-align: left;
-	white-space: pre;
+	width: 100%;
 }
 table > caption {
 	background: #f1f6fb;
@@ -65,6 +65,10 @@ table > caption {
 }
 table td, table th {
 	padding: .25rem;
+	white-space: pre-wrap;
+}
+table td {
+	overflow-wrap: anywhere;
 }
 table > tbody > tr:hover {
 	background: #f1f6fb;
